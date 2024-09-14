3387ac0361b77430b172fef252638efde5f5a5a0
Pranav Belligundu <pranav.belligundu@gmail.com>
2024-09-14 14:08:42 -0500

    some debugging

--- spiral-1.py
+++ spiral-1.py
@@ -14,57 +14,52 @@ import sys
 
 #THIS IS JUST A HELPER METHOD NO NEED TO DEBUG
 def create_blank_grid(length, width):
-    """HELPER METHOD"""
     grid = []
     for i in range(length):
         grid.append([])
-        for _ in range(width):
+        for j in range(width):
             grid[i].append(0)
-
+    
     return grid
 
 #THIS IS JUST A HELPER METHOD NO NEED TO DEBUG
 def print_grid(grid):
-    """HELPER METHOD"""
     printed_grid = create_blank_grid(len(grid), len(grid[0]))
+    
+    maxLength = [-1] * len(grid);
 
-    max_length = [-1] * len(grid)
-    length_grid0 = len(grid[0])
-    length_grid = len(grid)
-    for j in range(length_grid0):
-        for i in range(length_grid):
-            printed_grid[i][j] = str(grid[i][j]) + " "
-            if len(printed_grid[i][j]) > max_length[j]:
-                max_length[j] = len(printed_grid[i][j])
-
-    len_printed_grid = len(printed_grid)
-    len_printed_grid0 = len(printed_grid[0])
-    for i in range(len_printed_grid):
+    for j in range (len(grid[0])):
+        for i in range (len(grid)):
+            printed_grid[i][j] = (str(grid[i][j]) + " ")
+            if len(printed_grid[i][j]) > maxLength[j]:
+                maxLength[j] = len(printed_grid[i][j])
+         
+    for i in range (len(printed_grid)):
         row = ""
-        for j in range(len_printed_grid0):
-            row += (printed_grid[i][j] + " " * (max_length[j] - len(printed_grid[i][j]) + 1))
+        for j in range (len(printed_grid[0])): 
+            row += (printed_grid[i][j] + " " * (maxLength[j] - len(printed_grid[i][j]) + 1))
         print(row)
 
 
-def find_fib_number(one_prev, two_prev, multiplicity):
+def find_fib_number(onePrev, twoPrev, multiplicity):
     """Finds the Fibonacci number"""
-    limit = 100
-    fib_number = one_prev + two_prev
+    LIMIT = 100
+    fib_number = onePrev + twoPrev
 
-    if fib_number > limit:
+    if fib_number > LIMIT:
         fib_number = multiplicity
-        fib_number *= -1
+        return fib_number * -1
     return fib_number
 
 def check_valid_space(row, col, grid):
     """Checks the valid space"""  
     length = len(grid)
-    if row < 0 or row > length or col < 0 or col > length:
+    #if row < 0 or row > length or col < 0 or col > length: old code
+    if row < 0 or row >= length or col < 0 or col >= length:
         return False
     return grid[row][col] == 0
 
 def create_spiral(dim):
-    """Create Spiral"""
     # create variable to store our spiral numbers
     grid = create_blank_grid(dim, dim)
 
@@ -73,41 +68,44 @@ def create_spiral(dim):
         [1, 0],
         [0, -1],
         [-1, 0]
-    ]
-
-    cur_row = 0
-    cur_col = 0
-    two_prev = 0
-    one_prev = 1
-    cur_dir = 0
+    ]        
+    
+    curRow = 0
+    curCol = 0
 
 
-    for _ in range(dim ** 2):
+    twoPrev = 0
+    onePrev = 1
+
+    curDir = 0
+
+
+    for i in range(dim ** 2):
         multiplicity = 1
-        attempted_row = cur_row + directions[cur_dir][0]
-        attempted_col = cur_col + directions[cur_dir][1]
+        attemptedRow = curRow + directions[curDir][0]
+        attemptedCol = curCol + directions[curDir][1]
 
-        if check_valid_space(attempted_row, attempted_col, grid):
-            cur_row = attempted_row
-            cur_col = attempted_col
+        if check_valid_space(attemptedRow, attemptedCol, grid):
+            curRow = attemptedRow
+            curCol = attemptedCol
         else:
-            cur_dir =  cur_dir + 1
-            cur_row += directions[cur_dir][0]
-            cur_col += directions[cur_dir][1]
-            grid[cur_row][cur_col] = find_fib_number(one_prev, two_prev, multiplicity)
+            curDir =  curDir + 1
+            curRow += directions[curDir][0]
+            curCol += directions[curDir][1]
+            grid[curRow][curCol] = find_fib_number(onePrev, twoPrev, multiplicity)
 
-        if grid[cur_row][cur_col] >= one_prev:
-            two_prev = one_prev
-            one_prev = grid[cur_row][cur_col]
-        elif grid[cur_row][cur_col] == 0:
+        if grid[curRow][curCol] >= onePrev:
+            twoPrev = onePrev
+            onePrev = grid[curRow][curCol]
+        elif grid[curRow][curCol] == 0:
             break
         else:
             #we are flipping back the signal we set earlier to correct it
-            grid[cur_row][cur_col] *= -1
+            grid[curRow][curCol] *= -1
             multiplicity += 1
-            one_prev = 0
-            two_prev = multiplicity
-
+            onePrev = 0
+            twoPrev = multiplicity
+        
     print_grid(grid)
 
     return grid
@@ -140,20 +138,24 @@ def sum_sub_grid(grid, val):
 def main():
     """main with input through terminal"""
     while True:
-        try:
+        try: #try catch block for invalid input
             input1 = input()
-            if input1 != "":
-                dim = int(input1)
-                grid = create_spiral(dim)
+            if input1 == "":
+                break
+            dim = int(input1)
+            grid = create_spiral(dim)
+
             input2 = input()
-            if input2 != "":
-                sum_val = int(input2)
+            if input2 == "":
+                break
+            sum_val = int(input2)
+
             adj_sum = sum_sub_grid(grid, sum_val)
             print(adj_sum)
-            input3 = input()
-            if input3 == "":
-                break
+
         except ValueError:
+            #if invalid input
+            print("String Invalid Input")
             continue
 
 """
