e6403a72be0d15ba47dc2774789b9eaf84ae9a2c
SwordCraft-Hero <arunmsn19@gmail.com>
2024-11-04 08:18:14 -0600

    Few modifications

--- Week10/expression_tree.py
+++ Week10/expression_tree.py
@@ -1,21 +1,12 @@
+"""expression_tree.py"""
+
 #  File: ExpressionTree.py
-
-#  Description:
-
-#  Student Name:
-
-#  Student UT EID:
-
-#  Partner Name:
-
-#  Partner UT EID:
-
+#  Description: Working with an Expression Tree
+#  Student Name: Arun Mahadevan Sathia Narayanan
+#  Student UT EID: as235872
 #  Course Name: CS 313E
-
-#  Unique Number: 
-
-#  Date Created:
-
+#  Unique Number: 50184
+#  Date Created: 11/04/2024
 #  Date Last Modified:
 
 import sys
@@ -23,58 +14,69 @@ import sys
 operators = ['+', '-', '*', '/', '//', '%', '**']
 
 class Stack ():
+    """Stack class"""
     def __init__(self):
         self.stack = []
 
     def push(self, data):
+        """push"""
         self.stack.append (data)
 
     def pop(self):
-        if(not self.is_empty()):
+        """pop"""
+        if not self.is_empty():
             return self.stack.pop()
         else:
             return None
 
     def is_empty(self):
+        """is_empty"""
         return len(self.stack) == 0
 
 class Node ():
-    def __init__ (self, data = None, lChild = None, rChild = None):
+    """Node class"""
+    def __init__ (self, data = None, l_child = None, r_child = None):
         self.data = data
-        self.lChild = lChild
-        self.rChild = rChild
+        self.l_child = l_child
+        self.r_child = r_child
 
 class Tree():
+    """Tree class"""
     def __init__ (self):
         self.root = None
-    
-    # this function takes in the input string expr and 
+
+    # this function takes in the input string expr and
     # creates the expression tree
     def create_tree (self, expr):
-    
+        """creates tree"""
+
     # this function should evaluate the tree's expression
     # returns the value of the expression after being calculated
     def evaluate (self, aNode):
-    
-    # this function should generate the preorder notation of 
+        """evaluates expressions"""
+
+    # this function should generate the preorder notation of
     # the tree's expression
     # returns a string of the expression written in preorder notation
     def pre_order (self, aNode):
+        """preordering of elements"""
 
-    # this function should generate the postorder notation of 
+    # this function should generate the postorder notation of
     # the tree's expression
     # returns a string of the expression written in postorder notation
     def post_order (self, aNode):
+        """postordering of elements"""
 
 # you should NOT need to touch main, everything should be handled for you
 def main():
+    """main function"""
     # read infix expression
     line = sys.stdin.readline()
     expr = line.strip()
- 
+
     tree = Tree()
     tree.create_tree(expr)
-    
+
     # evaluate the expression and print the result
     print(expr, "=", str(tree.evaluate(tree.root)))
 
