39ea37ccf166b024144b83b42e1d381428985415
SwordCraft-Hero <arunmsn19@gmail.com>
2024-11-09 14:58:28 -0600

    Finished Assignment 9

--- Week10/expression_tree.py
+++ Week10/expression_tree.py
@@ -11,7 +11,7 @@
 
 import sys
 
-operators = ['+', '-', '*', '/', '//', '%', '**']
+OPERATORS = ['+', '-', '*', '/', '//', '%', '**']
 
 class Stack ():
     """Stack class"""
@@ -26,8 +26,7 @@ class Stack ():
         """pop"""
         if not self.is_empty():
             return self.stack.pop()
-        else:
-            return None
+        return None
 
     def is_empty(self):
         """is_empty"""
@@ -40,6 +39,14 @@ class Node ():
         self.l_child = l_child
         self.r_child = r_child
 
+    def get_r_child(self):
+        """gets the r child"""
+        return self.r_child()
+
+    def get_l_child(self):
+        """gets the l child"""
+        return self.l_child()
+
 class Tree():
     """Tree class"""
     def __init__ (self):
@@ -50,31 +57,95 @@ class Tree():
     def create_tree (self, expr):
         """creates tree"""
         elements = expr.split()
-        root = Node()
-        current = root
+        self.root = Node()
+        current = self.root
         equation = Stack()
         for token in elements:
             if token == "(":
                 current.l_child = Node()
                 equation.push(current)
                 current = current.l_child
+            elif token in OPERATORS:
+                current.data = token
+                equation.push(current)
+                current.r_child = Node()
+                current = current.r_child
+            elif token == ")":
+                if not equation.is_empty():
+                    current = equation.pop()
+            else:
+                if '.' in token:
+                    current.data = float(token)
+                else:
+                    current.data = int(token)
+                if not equation.is_empty():
+                    current = equation.pop()
 
     # this function should evaluate the tree's expression
     # returns the value of the expression after being calculated
     def evaluate (self, a_node):
         """evaluates expressions"""
+        if a_node.l_child is None and a_node.r_child is None:
+            return a_node.data
+
+        l_val = self.evaluate(a_node.l_child)
+        r_val = self.evaluate(a_node.r_child)
+
+        value = 0
+        if a_node.data == '+':
+            value = l_val + r_val
+        if a_node.data == '-':
+            value = l_val - r_val
+        if a_node.data == '*':
+            value = l_val * r_val
+        if a_node.data == '/':
+            value = l_val / r_val
+        if a_node.data == '//':
+            value = l_val // r_val
+        if a_node.data == '%':
+            value = l_val % r_val
+        if a_node.data == '**':
+            value = l_val ** r_val
+
+        return float(value)
 
     # this function should generate the preorder notation of
     # the tree's expression
     # returns a string of the expression written in preorder notation
     def pre_order (self, a_node):
         """preordering of elements"""
+        if a_node is None:
+            return ""
+
+        expr = str(a_node.data)
+        left = self.pre_order(a_node.l_child)
+        if left:
+            expr += " " + left
+        right = self.pre_order(a_node.r_child)
+        if right:
+            expr += " " + right
+
+        return expr
 
     # this function should generate the postorder notation of
     # the tree's expression
     # returns a string of the expression written in postorder notation
     def post_order (self, a_node):
         """postordering of elements"""
+        if a_node is None:
+            return ""
+
+        expr = ""
+        left = self.post_order(a_node.l_child)
+        if left:
+            expr += left + " "
+        right = self.post_order(a_node.r_child)
+        if right:
+            expr += right + " "
+
+        expr += str(a_node.data)
+
+        return expr
 
 # you should NOT need to touch main, everything should be handled for you
 def main():
